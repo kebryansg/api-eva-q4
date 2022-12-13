37dad2e10a664273bc805f2cf2bc299e92a49e48
Marco Pérez <marco.perezj96@gmail.com>
2022-12-13 12:59:16 -0500

    Actualización a validaciones de EndPoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { environments } from '../config/environments';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database.module';
 import { CatalogoModule } from './catalogo/catalogo.module';
 import { EstadoModule } from './estados/estado.module';
 import { RegistroModule } from './registro/registro.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: environments[process.env.NODE_ENV] || '.env',
-      isGlobal: true,
-    }),
-    DatabaseModule,
-    CatalogoModule,
-    EstadoModule,
-    RegistroModule,
-  ],
+  imports: [CatalogoModule, EstadoModule, RegistroModule],
   controllers: [AppController],
   providers: [AppService],
 })
