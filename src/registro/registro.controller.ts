6d519979dbe2273577b81b387c8cb578b4059969
mperezja <bpuio01610039lm@BPUIO01610200L.uio.bpichincha.com>
2022-12-14 12:28:22 -0500

    Added delay to register

--- src/registro/registro.controller.ts
+++ src/registro/registro.controller.ts
@@ -8,6 +8,7 @@ import {
   Req,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { resolve } from 'path';
 import { CreateFormDto } from './dto/create-form.dto';
 
 @ApiTags('Registro')
@@ -16,7 +17,8 @@ export class RegistroController {
   constructor() {}
 
   @Post()
-  create(@Body() createFormDto: CreateFormDto) {
+  async create(@Body() createFormDto: CreateFormDto) {
+    await this.delay(1500);
     if (Math.random() < 0.6) {
       return {
         statusCode: 'success',
@@ -28,5 +30,13 @@ export class RegistroController {
         HttpStatus.BAD_REQUEST,
       );
     }
+  }
+
+  private delay(time: number): Promise<boolean> {
+    return new Promise((resolve) => {
+      setTimeout(function () {
+        resolve(true);
+      }, time);
+    });
   }
 }
